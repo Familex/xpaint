49e7f49bfe0c8085c8064142ddd57d21c7cc0d9e
Familex <trener2990@gmail.com>
2023-12-10 15:00:15 +0300

    animation thread

--- xpaint.c
+++ xpaint.c
@@ -5,9 +5,30 @@
 #include <stdlib.h>
 #include <string.h>
 
+// temp solution
+#include <pthread.h>
+#include <unistd.h>
+
 char title[] = "xpaint";
+Bool done = False;
+
+struct SelectCircle {
+    Bool is_active;
+    int start_time_ms;
+    int x;
+    int y;
+} sel_circ = {0};
+
+struct AnimateArgs {
+    Display* display;
+    Drawable drawable;
+    GC gc;
+};
+
+void* animate(void*);
 
 int main(int argc, char** argv) {
+    pthread_t animate_thread_id;
     KeySym mykey;
     char text[10];
 
@@ -53,22 +74,34 @@ int main(int argc, char** argv) {
     );
 
     /* graphics context */
-    GC mygc = XCreateGC(display, window, 0, 0);
-    XSetBackground(display, mygc, mybackground);
-    XSetForeground(display, mygc, myforeground);
+    GC gc = XCreateGC(display, window, 0, 0);
+    XSetBackground(display, gc, mybackground);
+    XSetForeground(display, gc, myforeground);
 
     /* allow receiving mouse events */
     XSelectInput(
         display,
         window,
-        ButtonPressMask | KeyPressMask | ExposureMask
+        ButtonPressMask | ButtonReleaseMask | KeyPressMask | ExposureMask
     );
 
     /* show up window */
     XMapRaised(display, window);
 
+    /* FIXME animation thread start */
+    struct AnimateArgs animate_thread_args = {
+        .display = display,
+        .drawable = window,
+        .gc = gc,
+    };
+    pthread_create(
+        &animate_thread_id,
+        NULL,
+        animate,
+        (void*)&animate_thread_args
+    );
+
     /* event loop */
-    Bool done = False;
     while (!done) {
         /* fetch event */
         XEvent event = {0};
@@ -81,7 +114,7 @@ int main(int argc, char** argv) {
                     XDrawImageString(
                         event.xexpose.display,
                         event.xexpose.window,
-                        mygc,
+                        gc,
                         50,
                         50,
                         title,
@@ -93,14 +126,29 @@ int main(int argc, char** argv) {
                 XRefreshKeyboardMapping(&event.xmapping);
             } break;
             case ButtonPress: {
+                XButtonPressedEvent* e = (XButtonPressedEvent*)&event;
+                if (e->button == Button3) {
+                    sel_circ.is_active = True;
+                    sel_circ.start_time_ms = e->time;
+                    sel_circ.x = e->x;
+                    sel_circ.y = e->y;
+                }
+            } break;
+            case ButtonRelease: {
+                XButtonReleasedEvent* e = (XButtonReleasedEvent*)&event;
+                if (e->button == Button3) {
+                    sel_circ.is_active = False;
+                }
             } break;
             case KeyPress: {
                 /* Key input. */
                 int i = XLookupString(&event.xkey, text, 10, &mykey, 0);
-                if (i == 1 && text[0] == 'q')
+                if (i == 1 && text[0] == 'q') {
                     done = True;
+                }
             } break;
             case DestroyNotify: {
+                // FIXME feels useless
                 XDestroyWindowEvent* e = (XDestroyWindowEvent*)&event;
                 if (e->window == window) {
                     done = True;
@@ -110,9 +158,23 @@ int main(int argc, char** argv) {
     }
 
     /* finalization */
-    XFreeGC(display, mygc);
+    XFreeGC(display, gc);
     XDestroyWindow(display, window);
     XCloseDisplay(display);
 
+    pthread_join(animate_thread_id, NULL);
+
     exit(0);
+}
+
+void* animate(void* args_v) {
+    struct AnimateArgs* args = (struct AnimateArgs*)args_v;
+
+    while (!done) {
+        XDrawRectangle(args->display, args->drawable, args->gc, 30, 30, 40, 30);
+
+        sleep(2);
+    }
+
+    pthread_exit(NULL);
 }
