0d96cc81270b9d00612060e06e13d3f6a4b09e34
Familex <trener2990@gmail.com>
2023-12-10 17:54:05 +0300

    circle

--- xpaint.c
+++ xpaint.c
@@ -1,3 +1,5 @@
+#define _POSIX_C_SOURCE 200809L
+
 #include <X11/X.h>
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
@@ -7,17 +9,23 @@
 
 // temp solution
 #include <pthread.h>
+#include <time.h>
 #include <unistd.h>
 
 char title[] = "xpaint";
 Bool done = False;
+unsigned long ANIMATE_SLEEP_MS = 16;
+unsigned long MIN_SEL_RECT_DIMENTION_PX = 50;
+unsigned long MAX_SEL_RECT_DIMENTION_PX = 200;
+unsigned long SEL_CIRC_ANIMATION_TIME_MS = 1000;
 
 struct SelectCircle {
     Bool is_active;
-    int start_time_ms;
+    struct timespec start_time;
     int x;
     int y;
 } sel_circ = {0};
+pthread_mutex_t sel_circ_mtx;
 
 struct AnimateArgs {
     Display* display;
@@ -26,6 +34,8 @@ struct AnimateArgs {
 };
 
 void* animate(void*);
+XRectangle get_curr_sel_rect();
+struct timespec get_time();
 
 int main(int argc, char** argv) {
     pthread_t animate_thread_id;
@@ -128,10 +138,12 @@ int main(int argc, char** argv) {
             case ButtonPress: {
                 XButtonPressedEvent* e = (XButtonPressedEvent*)&event;
                 if (e->button == Button3) {
+                    pthread_mutex_lock(&sel_circ_mtx);
                     sel_circ.is_active = True;
-                    sel_circ.start_time_ms = e->time;
+                    sel_circ.start_time = get_time();
                     sel_circ.x = e->x;
                     sel_circ.y = e->y;
+                    pthread_mutex_unlock(&sel_circ_mtx);
                 }
             } break;
             case ButtonRelease: {
@@ -169,12 +181,74 @@ int main(int argc, char** argv) {
 
 void* animate(void* args_v) {
     struct AnimateArgs* args = (struct AnimateArgs*)args_v;
+    static Bool sel_circ_flush_done = False;
 
     while (!done) {
-        XDrawRectangle(args->display, args->drawable, args->gc, 30, 30, 40, 30);
+        if (sel_circ.is_active || !sel_circ_flush_done) {
+            XRectangle sel_rect = get_curr_sel_rect();
 
-        sleep(2);
+            XClearArea(
+                args->display,
+                args->drawable,
+                sel_rect.x - 1,
+                sel_rect.y - 1,
+                sel_rect.width + 2,
+                sel_rect.height + 2,
+                // Expose to draw background
+                True
+            );
+
+            if (!sel_circ_flush_done) {
+                sel_circ_flush_done = True;
+            } else {
+                sel_circ_flush_done = False;
+                // draw circle
+                XDrawArc(
+                    args->display,
+                    args->drawable,
+                    args->gc,
+                    sel_rect.x,
+                    sel_rect.y,
+                    sel_rect.width,
+                    sel_rect.height,
+                    0,
+                    360 * 64
+                );
+                XFlush(args->display);
+            }
+        }
+
+        struct timespec ts = {
+            .tv_sec = 0,
+            .tv_nsec = ANIMATE_SLEEP_MS,
+        };
+        nanosleep(&ts, &ts);
     }
 
     pthread_exit(NULL);
+}
+
+XRectangle get_curr_sel_rect() {
+    pthread_mutex_lock(&sel_circ_mtx);
+    // FIXME
+    short dimention = 200;
+
+    XRectangle result = {
+        .x = sel_circ.x - dimention / 2,
+        .y = sel_circ.y - dimention / 2,
+        .height = dimention,
+        .width = dimention,
+    };
+
+    pthread_mutex_unlock(&sel_circ_mtx);
+
+    return result;
+}
+
+struct timespec get_time() {
+    struct timespec result;
+
+    clock_gettime(CLOCK_REALTIME, &result);
+
+    return result;
 }
