Familex/xpaint

commit e5833f6e59fd56c02cfe43d60dbc7991076cb352

clear overlay on button_press_hdlr
it fixes visual bug with figure animations if RMB pressed while LMB pressed

xpaint.c
@@ -4740,6 +4740,7 @@ HdlrResult button_press_hdlr(struct Ctx* ctx, XEvent* event) {
     inp->damage = RNIL;
     inp->redraw_track[0] = RNIL;
     inp->redraw_track[1] = RNIL;
+    overlay_clear(&ctx->input.ovr);
 
     if (inp->mode.t == InputT_Transform || inp->mode.t == InputT_Text) {
         // do nothing
@@ -4751,7 +4752,7 @@ HdlrResult button_press_hdlr(struct Ctx* ctx, XEvent* event) {
         input_set_damage(inp, curr_damage);
     }
 
-    update_screen(ctx, (Pair) {e->x, e->y}, False);
+    update_screen(ctx, (Pair) {e->x, e->y}, True);
     draw_selection_circle(ctx, &ctx->sc, e->x, e->y);
 
     inp->c = (struct CursorState) {

commit c9dd4a70106b9c65f1417af4421b045261c46eb3

regression: fix transform mode (introduced in e5833f6)

xpaint.c
@@ -5012,13 +5012,13 @@ HdlrResult button_press_hdlr(struct Ctx* ctx, XEvent* event) {
     inp->damage = RNIL;
     inp->redraw_track[0] = RNIL;
     inp->redraw_track[1] = RNIL;
-    overlay_clear(&ctx->input.ovr);
 
     if (inp->mode.t == InputT_Transform || inp->mode.t == InputT_Text) {
         // do nothing
     } else if (inp->c.state == CS_None && (BTN_EQ(button, BTN_SEL_CIRC) | BTN_EQ(button, BTN_SEL_CIRC_ALTERNATIVE))) {
         sel_circ_init_and_show(ctx, button, e->x, e->y);
     } else if (tc->on_press) {
+        overlay_clear(&ctx->input.ovr);
         Rect const curr_damage = tc->on_press(ctx, e);
         overlay_expand_rect(&inp->ovr, curr_damage);
         input_set_damage(inp, curr_damage);